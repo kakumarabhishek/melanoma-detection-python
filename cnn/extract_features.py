1721f40ee5f705972f381fe228176a07a50a8da7
hliaqat <hliaqat@sfu.ca>
2021-05-08 07:43:35 -0700

    regression

--- cnn/extract_features.py
+++ cnn/extract_features.py
@@ -27,6 +27,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from keras.utils.np_utils import to_categorical
 from keras import layers
+import tensorflow as tf
 
 # load the network
 print("[INFO] loading network...")
@@ -48,15 +49,18 @@ imagePaths = list(paths.list_images(p))
 
 imageData = []
 labels = []
+imageNames = []
 
 desired_size = 256
 
+count = 0
 print("[INFO] processing images...")
 for imagePath in imagePaths:
 	# load the input image using the Keras helper utility
-	# while ensuring the image is resized to 192x256 pixels
 	image = load_img(imagePath)
 	image = np.array(image)
+
+	# resize image to desired_size while padding to maintain aspect ratio
 	old_size = image.shape
 	ratio = float(desired_size)/max(old_size)
 	new_size = tuple([int(x*ratio) for x in old_size])
@@ -75,7 +79,7 @@ for imagePath in imagePaths:
 	image = img_to_array(image)
 	image = np.array(image)
 
-	# Testing conversion to greyscale
+	# Conversion to greyscale
 	image = rgb2gray(image)
 	image = np.repeat(image[..., np.newaxis], 3, -1)
 
@@ -92,63 +96,31 @@ for imagePath in imagePaths:
 	imageName = imagePath.split(os.path.sep)[7]
 	file_of_feature_to_predict = os.path.sep.join(["../results",
 												   "{}.json".format(os.path.splitext(imageName)[0])])
+	imageNames.append(imageName)
 
 	with open(file_of_feature_to_predict) as f:
 		label_data = json.load(f)
-	# derive the D1 ground truth feature and add it to label list
-	# d1 = label_data['A1']
-	# d2 = label_data['A2']
-	# l = 0
-	# # data becomes sparse greater than 24, so set them as 25 to represent 'large' category
-	# if d1 > 0.15:
-	# 	l = l + 1
-	# if d2 > 0.15:
-	# 	l = l + 1
 
+	# Get Diameter data from json
 	d1 = label_data['D1']
-	print("d1: " + str(d1) + ", ratio: " + str(ratio))
+	# Program that generates json divides the pixel diameter by 72. Multiplying by 72 to obtain original pixel diameter
 	d1 = (72 * d1) * ratio
-	print(str(d1))
+	# print("d1: " + str(d1) + ", ratio: " + str(ratio) + ", image:" + imageName)
 	d1 = round(d1)
-	# print(d1)
-	# exit()
 	labels.append(d1)
 
-permLabels = labels
+imageNames = np.array(imageNames)
+labels = np.array(labels)
+
 numLabels = len(set(labels))
 
-print("[INFO] printing count of labels...")
-occ_dict = {}
-for item in labels:
-    if item not in occ_dict:
-        occ_dict[item] = 1
-    else:
-        occ_dict[item] +=1
-
-print(occ_dict)
-
-print("[INFO] encoding labels...")
-print(numLabels)
-# label_encoder = LabelEncoder()
-# labels = label_encoder.fit_transform(labels)
-
-# print("[INFO] printing encoded labels...")
-# print(labels[1])
-# print(labels[2])
-# print(labels[3])
-
-# labels = to_categorical(labels)
-
-# print("[INFO] printing categorized labels...")
-# print(labels[1])
-# print(labels[2])
-# print(labels[3])
-
-# imageData = imageData / 255.0
 print("[INFO] splitting data...")
 trainX, testX, trainY, testY = train_test_split(imageData, labels, test_size=0.2, random_state=1)
-
 trainX, valX, trainY, valY = train_test_split(trainX, trainY, test_size=0.25, random_state=1)
+
+# Used to keep track of imageNames for test labels
+imagetrainX, imagetestX, imagetrainY, imagetestY = train_test_split(imageData, imageNames, test_size=0.2, random_state=1)
+imagetrainX, imagevalX, imagetrainY, imagevalY = train_test_split(imagetrainX, imagetrainY, test_size=0.25, random_state=1)
 
 trainX = np.array(trainX)
 trainY = np.array(trainY)
@@ -172,7 +144,7 @@ x = layers.GlobalMaxPooling2D()(last_output)
 x = layers.Dense(512, activation='relu')(x)
 # Add a dropout rate of 0.5
 x = layers.Dropout(0.5)(x)
-# Add a final sigmoid layer for classification
+# Add a final linear layer for regression
 x = layers.Dense(1, activation="linear")(x)
 
 model = Model(pre_trained_model.input, x)
@@ -225,7 +197,7 @@ history = model.fit_generator(train_datagen.flow(trainX,trainY, batch_size=batch
                               validation_steps=(valX.shape[0] // batch_size), callbacks=[learning_rate_reduction])
 
 model.save('model/diameterNormalizedRegression')
-# model = models.load_model('model/diameterNormalized')
+# model = models.load_model('model/diameterNormalizedRegression')
 
 loss_val, acc_val = model.evaluate(valX, valY, verbose=1)
 print("Validation: accuracy = %f  ;  loss_v = %f" % (acc_val, loss_val))
@@ -233,49 +205,25 @@ print("Validation: accuracy = %f  ;  loss_v = %f" % (acc_val, loss_val))
 loss_test, acc_test = model.evaluate(testX, testY, verbose=1)
 print("Test: accuracy = %f  ;  loss = %f" % (acc_test, loss_test))
 
-import tensorflow as tf
-
 classes = model.predict(testX, verbose=1)
-
-print("[INFO] printing predicted labels...")
-print(classes[1])
-print(classes[2])
-print(classes[3])
-
-# classes = tf.argmax(classes, axis=1)
-
-# print("[INFO] printing argmaxed classes...")
-# print(classes[1])
-# print(classes[2])
-# print(classes[3])
-
-# # # classes = label_encoder.inverse_transform(classes)
-
-# testLabels = tf.argmax(testY, axis=1)
-
-# print("[INFO] printing argmaxed testLabels...")
-# print(testLabels[1])
-# print(testLabels[2])
-# print(testLabels[3])
-
-# # # testLabels = label_encoder.inverse_transform(testLabels)
 
 classes = np.array(classes)
 testLabels = np.array(testY)
+imageLabels = np.array(imagetestY)
 
-print("[INFO] printing argmaxed classes 2...")
+print("[INFO] printing classes ...")
 print(classes.shape)
 print(classes[1])
 print(classes[2])
 print(classes[3])
 
-print("[INFO] printing argmaxed testLabels 2...")
+print("[INFO] printing testLabels ...")
 print(testLabels.shape)
 print(testLabels[1])
 print(testLabels[2])
 print(testLabels[3])
 
 
-output = np.column_stack((classes.flatten(),testLabels.flatten()))
-np.savetxt('outputRegression.csv', output, delimiter=',', header="Predicted, Label")
+output = np.column_stack((classes.flatten(),testLabels.flatten(), imageLabels.flatten()))
+np.savetxt('outputRegressionNames.csv', output, delimiter=',', header="Predicted, Label, Image", fmt='%s')
 
